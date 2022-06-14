0e7115fc484f39d46426d922d1cdb7a699819eef
delanyeong <delan.april@gmail.com>
2022-06-14 12:13:30 +0800

    difference between for loop and lambda expression

--- src/main/java/vttp2022/day07/ColMain.java
+++ src/main/java/vttp2022/day07/ColMain.java
@@ -9,16 +9,18 @@ public class ColMain {
     public static void main(String[] args) {
         Random rand = new SecureRandom();
         List<Integer> myList = new LinkedList<>();
-        for (int i = 0 ; i <100 ; i++) 
+        for (int i = 0 ; i <5 ; i++) 
         myList.add(rand.nextInt(100));
 
         System.out.println(myList);
 
-        
+
+        for (int i = 0 ; i < myList.size() ; i++)
+        System.out.printf(" %d\n", myList.get(i));
 
         myList.stream()
         .forEach((value)->{
-            System.out.printf(">>> %d\n", value);
+            System.out.printf("forEach %d\n", value);
         });
     }
 }
